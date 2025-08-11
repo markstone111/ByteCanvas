bf11d0c928168eb5184d894ae0c2e745abe76c3a
Nikunj Maheshwari <nikunjnehu@gmail.com>
2025-08-11 10:32:48 +0530

    03: Authentication

---
+++ app/(auth)/auth/sign-in/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from 'next/image'
+import SignInFormClient from '@/features/auth/components/signin-form-client'
+
+const SignInPage = () => {
+  return (
+    <div className='space-y-6 flex flex-col items-center justify-center'>
+        <Image src={"/logo-side_end.dev.png"} alt="ByteCanvas Logo" width={100} height={100} className='rounded-full'/>
+        <SignInFormClient/>
+    </div>
+  )
+}
+
+export default SignInPage
---
+++ middleware.ts
@@ -0,0 +1,37 @@
+import NextAuth from "next-auth";
+
+import authConfig from "./auth.config";
+import { apiAuthPrefix, authRoutes, DEFAULT_LOGIN_REDIRECT, publicRoutes } from "./routes";
+import { Urbanist } from "next/font/google";
+
+const { auth } = NextAuth(authConfig);
+
+export default auth((req)=>{
+    const {nextUrl} = req;
+    const isLoggedIn = !!req.auth;
+
+    const isAPiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+    if(isAPiAuthRoute) return null;
+
+    if(isAuthRoute){
+        if(isLoggedIn){
+            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+        }
+        return null;
+    }
+
+    if(!isLoggedIn && !isPublicRoute){
+        return Response.redirect(new URL("/auth/sign-in", nextUrl));
+    }
+    return null;
+
+});
+
+export const config ={
+    matcher:["/((?!.+\\.[\\w]+$|_next).*)","/","/(api|trpc)(.*)"],
+}
